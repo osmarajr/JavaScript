f71fdf6e31685d0bd5150b7cd5ff9ffd818f5236
jose osmar <osmar.ajr10@gmail.com>
2024-01-30 22:51:33 -0300

    first commit

---
+++ js/alert.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+//alert("Sou JavaScript!")
+alert("Hello");
+alert("World!")
+
+
+let message;
+message = "teste da variável";
+
+alert(message);
+
+let message2 = "Teste de variável 2";
+alert(message2);
+
+*/
+let user = 'john',
+    age = 25,
+    message = 'Hello!';
+
+alert(message + " Seu nome é " + user + ", possui "+ age + " anos");
