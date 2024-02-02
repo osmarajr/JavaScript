37f52c73e78b7398782500b0478ff15f8eb79c44
jose osmar <osmar.ajr10@gmail.com>
2024-02-02 17:43:17 -0300

    uso do ??

--- js/alert.js
+++ js/alert.js
@@ -13,9 +13,14 @@ alert(message);
 let message2 = "Teste de variável 2";
 alert(message2);
 
-*/
+
 let user = 'john',
     age = 25,
     message = 'Hello!';
 
 alert(message + " Seu nome é " + user + ", possui "+ age + " anos");
+*/
+// Uso do ?? 
+let user = null;
+
+alert(user ?? "Anonymous");
